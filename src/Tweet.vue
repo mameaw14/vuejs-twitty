19cbb72327a46c8148013e367f529a0a716a2bb3
mameaw14 <pinyapornster@gmail.com>
2016-12-03 21:12:40 +0700

    component

---
+++ src/Tweet.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="x in text">
+            <b>@{{username}}: </b>
+            {{x}}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                username: 'Manow',
+                text: [
+                    '12345',
+                    'test',
+                    '3ช่ามาแรง'
+                ]
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './Home.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '/', component: Home}
+  ]
+})
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
